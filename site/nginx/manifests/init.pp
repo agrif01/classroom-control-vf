8b1f4e09d0b69f89a77091e3995151643c50f6db
shawnh13 <shawn@duchessblue.com>
2017-02-08 15:04:44 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -13,17 +13,17 @@ class nginx {
   file {'nginx.conf':
     ensure => file,
     path   => '/etc/nginx/nginx.conf',
-    source => '${filesrc}/nginx.conf',
+    source => "${filesrc}/nginx.conf",
   }
   file {'default.conf':
     ensure => file,
     path   => '/etc/nginx/conf.d/default.conf',
-    source => '${filesrc}/default.conf',
+    source => "${filesrc}/default.conf",
   }
   file {'index.html':
     ensure => file,
     path   => '/var/www/index.html',
-    source => '${filesrc}/index.html',
+    source => "${filesrc}/index.html",
   }
   file {'/var/www':
     ensure => directory,
