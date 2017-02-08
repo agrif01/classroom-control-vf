0e31de014564c5ef6493eb0e6e13df3ead190159
jkwon12 <joe.kwong@safeway.com>
2017-02-08 10:08:56 -0800

    Create init.pp

---
+++ site/users/manifests/init.pp
@@ -0,0 +1,9 @@
+/etc/puppetlabs/code/environments/production/modules/ssh/manifests/init.pp
+
+class users {
+user { 'fundamentals':
+  ensure => present,
+# password => 'puppet8#labs', # Windows requires a plain text password
+# groups => ['Users'], # Display in Windows Control Panel
+}
+}
