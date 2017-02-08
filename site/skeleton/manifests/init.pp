170dbbac4ad6003ace86fe23e4be48237d75244c
MaeCruz <maedeliza.cruz@safeway.com>
2017-02-09 03:42:11 +0800

    Lab 8.3 - modification ad source

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -9,6 +9,7 @@ class skeleton {
          ensure => file,
          owner  => 'root',
          group  => 'root',
-         mode   => '0644'
+         mode   => '0644',
+         source => 'puppet:///modules/skeleton/files.bashrc',
        }
 }
