3ef4c5573e1b57cc789f5d0be4d7c67064308383
acapi12016 <aecapigon@gmail.com>
2017-02-09 05:49:41 +0800

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,23 +1,47 @@
 class memcached {
 
-  file { '/etc/sysconfig/memcached':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'],
+  package { 'memcached' :
+    ensure => present,
   }
   
-  package { 'memcached':
-  ensure => present,
-  before => File ['/etc/sysconfig/memcached']
+  file { '/etc/sysconfig/memcached' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    notify  => Service['memcached'],
   }
   
-  service { 'memcached':
-  ensure => running,
-  enable => true,
-  subscribe => File['/etc/sysconfig/memcached'],
+  service { 'memcached' :
+    ensure => running,
+    enable => true,
   }
 
+
 }
+
+#class memcached {
+
+#  file { '/etc/sysconfig/memcached':
+#  ensure => file,
+#  owner => 'root',
+#  group => 'root',
+#  mode => '0644',
+#  source => 'puppet:///modules/memcached/memcached',
+#  require => Package['memcached'],
+#  }
+  
+#  package { 'memcached':
+#  ensure => present,
+#  before => File ['/etc/sysconfig/memcached']
+#  }
+  
+#  service { 'memcached':
+#  ensure => running,
+#  enable => true,
+#  subscribe => File['/etc/sysconfig/memcached'],
+#  }
+
+#}
