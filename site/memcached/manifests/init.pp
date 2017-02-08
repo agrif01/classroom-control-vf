e20800ac77672be4e90d0b347b2b5f969e7cd320
acapi12016 <aecapigon@gmail.com>
2017-02-09 04:24:13 +0800

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+file { '/etc/sysconfig/memcached.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/ntp/ntp.conf',
+require => Package['ntp'],
+}
+package { 'memcached':
+ensure => present,
+}
+service { 'memcached':
+ensure => running,
+enable => true,
+subscribe => File['/etc/sysconfig/memcached.conf'],
+}
