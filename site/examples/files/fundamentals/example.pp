8b892ac19580e9acd3c5166777b44ded579439d7
skura01 <sireesha.kurapati@safeway.com>
2017-02-07 21:35:20 -0800

    Update example.pp

--- site/examples/files/fundamentals/example.pp
+++ site/examples/files/fundamentals/example.pp
@@ -1,10 +1,3 @@
-file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Hey, Puppet is fun!\n",
-}
 
 package { 'cowsay':
   ensure   => present,
