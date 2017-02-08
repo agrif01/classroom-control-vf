09bf041f36e43a62466810109d628fc3bc22c175
shawnh13 <shawn@duchessblue.com>
2017-02-08 12:41:23 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,19 +1,19 @@
 class nginx {
   package {'nginx':
-    ensure => 'installed',
+    ensure => present,
   }
   file {'nginx.conf':
-    ensure => 'file',
+    ensure => file,
     path   => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   file {'default.conf',
-    ensure => 'file',
+    ensure => file,
     path   => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
   }
   file {'index.html':
-    ensure => 'file',
+    ensure => file,
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
