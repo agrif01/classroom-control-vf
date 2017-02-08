8fc5ddb46922e38f73df72227d7d2e87a6fb772e
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-08 11:53:51 -0800

    Update init.pp

--- site/skelton/manifests/init.pp
+++ site/skelton/manifests/init.pp
@@ -1,16 +1,19 @@
 class skelton {
-  file { '/etc/skel':
-        ensure => directory,
-        owner => root,
-        group => root,
-        mode => 755
-        }
-  file { '/etc/skel/.bashrc':
-        ensure => file,
-        owner => root,
-        group => root,
-        mode => 0644
-        #source => 'puppet:///modules/skelton/bashrc',
-        source => 'puppet:///modules/skelton/bashrc',
-        }
-     }
+
+  file { '/etc/skel' :
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  
+  file { '/etc/skel/.bashrc' :
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skelton/bashrc',
+  }
+
+
+}
