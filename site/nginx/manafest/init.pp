4ab78245728c35d858e6a6f7c0a74c659bfd0412
agrif01 <agrif01@safeway.com>
2017-02-08 15:02:01 -0700

    added resource Default

--- site/nginx/manafest/init.pp
+++ site/nginx/manafest/init.pp
@@ -1,41 +1,34 @@
 class nginx {
+
+FILE {
+        owner => 'root',
+        group =>' root' ,
+        mode => '0664' ,
+     }
   package { 'nignx':
         ensure => present,
      {
   file { '/var/www' :
         ensure => directory,
-        owner => 'root',
-        group =>' root' ,
         mode => '0755' ,
      }
  file { '/var/www/index.html' :
         ensure => file,
-        owner => 'root',
-        group =>' root' ,
-        mode => '0644' ,
         source =>'puppet:///modules/nginx/index.html' ,
      }
  file { '/etc/nginx/nginx.conf':
        ensure => file ,
-       owner => 'root' ,
-       group => 'root' ,
-       mode => '0664' ,
-       source => 'puppet:///modules/nginx/nginx.conf' ,
+        source => 'puppet:///modules/nginx/nginx.conf' ,
        require => Package['nginx'] ,
        notify => Service['nginx'] ,
     }
     
    file { '/etc/nginx/conf.d':
 ensure => directory,
-owner => 'root',
-group => 'root',
 mode => '0775',
 }
 file { '/etc/nginx/conf.d/default.conf':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
 source => 'puppet:///modules/nginx/default.conf',
 require => Package['nginx'],
 notify => Service['nginx'],
