23e9b017df4d80081d925fa24928887dfef35788
jkwon12 <joe.kwong@safeway.com>
2017-02-08 14:45:11 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,10 +45,6 @@ node default {
   include role::classroom
   notify { "The fqdn of this machine is: ${::fqdn}" : } 
 
-# This is where you can declare classes for all nodes.
-# Example:
-# class { 'my_class': }
-notify { "Hello, my name is ${::hostname}": }
 
 file { '/etc/motd':
 ensure => file,
