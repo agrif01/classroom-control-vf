e0e7e895587c0f0389c9f01793b06e214cfabdc7
sredolme <SheilaMarie.Redolme@safeway.com>
2017-02-09 06:24:39 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,20 +38,30 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
-  notify { "The fqdn of this machine is: ${::fqdn}" : } 
-  
-  file { '/etc/motd' :
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => 'Applying changes from Github!\n', 
+#  include role::classroom
+#  notify { "The fqdn of this machine is: ${::fqdn}" : } 
+
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+  if $::virtual != 'physical' {
+  $vmname = capitalize($::virtual)
+  notify { "This is a ${vmname} virtual machine.": }
   }
+  
+
+#  file { '/etc/motd' :
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => '0644',
+#    content => 'Applying changes from Github!\n', 
+#  }
   
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
     path    => '/usr/bin:/usr/local/bin',
