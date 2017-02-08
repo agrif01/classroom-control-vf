b26caf8b7d81ce388577ed18d62276767ebeebab
agrif01 <agrif01@safeway.com>
2017-02-08 15:39:05 -0700

    redo

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,42 @@
+class nginx {
+
+FILE {
+        owner => 'root',
+        group =>' root' ,
+        mode => '0664' ,
+     }
+  package { 'nignx':
+        ensure => present,
+     {
+  file { '/var/www' :
+        ensure => directory,
+        mode => '0755' ,
+     }
+ file { '/var/www/index.html' :
+        ensure => file,
+        source =>'puppet:///modules/nginx/index.html' ,
+     }
+ file { '/etc/nginx/nginx.conf':
+       ensure => file ,
+        source => 'puppet:///modules/nginx/nginx.conf' ,
+       require => Package['nginx'] ,
+       notify => Service['nginx'] ,
+    }
+    
+   file { '/etc/nginx/conf.d':
+   ensure => directory,
+   mode => '0775',
+   }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+   ensure => running,
+   enable => true,
+  }
+}
+
+
