4994c2395b8f305db953c76132db415914b88e30
agrif01 <agrif01@safeway.com>
2017-02-08 15:21:10 -0700

    exercise 12.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,16 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  if $::virtual != 'physical' {
+     $vmname = capitalize($::virtual)
+     notify { "This is a ${vmname} virtual machine.": }
+   }
+  
+  
+  
+  
+  
+  
   include role::classroom
   notify { "The fqdn of this machine is: ${::fqdn}" : } 
  
