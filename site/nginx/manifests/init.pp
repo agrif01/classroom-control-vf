35fdc8c4d970b760cf2bda53b36e928b392ae6cf
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-08 14:02:14 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,30 +1,26 @@
 class nginx {
 
+File {
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+}
 package { 'nginx' :
   ensure => present,
 }
 
 file { '/var/www' :
   ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0755',
-}
+  }
 
 file { '/var/www/index.html' :
   ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  source => 'puppet:///modules/nginx/index.html',
+    source => 'puppet:///modules/nginx/index.html',
 }
   
 
 file { '/etc/nginx/nginx.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/nginx.conf',
   require =>  Package['nginx'],
   notify  => Service['nginx'],
@@ -33,9 +29,6 @@ file { '/etc/nginx/nginx.conf' :
 
 file { '/etc/nginx/conf.d/default.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
   notify  => Service['nginx'],
