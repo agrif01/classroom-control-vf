5dfeed59855131493a10215c1365aae4652b6ef3
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-08 10:51:08 -0800

    class skelton

---
+++ site/skelton/manifests/init.pp
@@ -0,0 +1,15 @@
+class skelton {
+  file { '/etc/skel':
+        ensure => directory,
+        owner => root,
+        group -> root,
+        mode => 755
+        }
+  file { '/etc/skel/.bashrc':
+        ensure => file,
+        owner => root,
+        group => root,
+        mode -> 0644
+        source => 'puppet:///modules/skelton/bashrc',
+        }
+     }
