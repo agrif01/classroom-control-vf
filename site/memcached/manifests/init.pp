1867c6ea88673c81ca58caecd3a19246f7d9654c
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-08 12:19:34 -0800

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -7,7 +7,7 @@ file { '/etc/sysconfig/memcached.conf':
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  source  => 'puppet:///modules/memcached/memcached.conf',
+  source  => 'puppet:///modules/memcached/memcached',
   require => Package['memcached'],
 }
 
