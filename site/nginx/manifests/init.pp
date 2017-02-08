423058e7b5e5765372a56da9fc1d15367931cc6d
Shobhna Shastri <shobhna@puppet.com>
2017-02-08 14:01:52 -0800

    Added a resource default declaration

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,30 +1,28 @@
 class nginx {
 
+File {
+  owner => 'root',
+  group => 'root',
+  mode =>  '0644',
+}
+
 package { 'nginx' :
   ensure => present,
 }
 
 file { '/var/www' :
   ensure => directory,
-  owner  => 'root',
-  group  => 'root',
   mode   => '0755',
 }
 
 file { '/var/www/index.html' :
   ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
   source => 'puppet:///modules/nginx/index.html',
 }
   
 
 file { '/etc/nginx/nginx.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/nginx.conf',
   require =>  Package['nginx'],
   notify  => Service['nginx'],
@@ -33,9 +31,6 @@ file { '/etc/nginx/nginx.conf' :
 
 file { '/etc/nginx/conf.d/default.conf' :
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
   notify  => Service['nginx'],
