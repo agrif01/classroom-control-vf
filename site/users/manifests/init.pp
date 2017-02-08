1ffacc150fb8e19072220b3556a3a057a4db6e28
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-08 10:16:10 -0800

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,4 +1,4 @@
-class site::users {     
+class users::fundamentals {     
               user { 'fundamentals': 
               ensure => present,     
               notify { "User was created": }
