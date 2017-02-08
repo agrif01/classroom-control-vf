5b4df15ada3aabe89cdaff1b1a48915b425844a4
acapi12016 <aecapigon@gmail.com>
2017-02-09 05:15:01 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,50 @@
 class nginx {
+
+package { 'nginx' :
+  ensure => present,
+}
+
+file { '/var/www' :
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+file { '/var/www/index.html' :
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  source => 'puppet:///modules/nginx/index.html',
+}
   
-  user {'nginx';
-    ensure => present,
-    }
+
+file { '/etc/nginx/nginx.conf' :
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  source  => 'puppet:///modules/nginx/nginx.conf',
+  require =>  Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+
+file { '/etc/nginx/conf.d/default.conf' :
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  source  => 'puppet:///modules/nginx/default.conf',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx' :
+  ensure => running,
+  enable => true,
+}
+
+
 }
