0441f6751a84f061ec0a3417a3db7f5686dde1d2
MaeCruz <maedeliza.cruz@safeway.com>
2017-02-09 04:54:20 +0800

    correction 1

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -9,14 +9,14 @@ class memcached {
       owner   => root,
       group   => root,
       mode    => '0644',
-      source  => 'puppet://modules/memcached/memcached',
-      require => package['memcached'],
+      source  => 'puppet:///modules/memcached/memcached',
+      require => Package['memcached'],
    }
    
    service { 'memcached' :
       ensure     => running,
       enable     => true,
-      subscribe  => file['/etc/sysconfig/memcached'],
+      subscribe  => File['/etc/sysconfig/memcached'],
    }
 
 }
