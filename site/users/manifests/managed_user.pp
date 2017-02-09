c97ba53c58b4a5f73e118465c3c9621270cc5ae9
skura01 <sireesha.kurapati@safeway.com>
2017-02-09 11:07:07 -0800

    Lab 15.1

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,23 +1,17 @@
 define users::managed_user (
-$user = $title1, $group = $title2
-)
-{
-user { $user : 
-  ensure=> present,
-  home => "/home/${user}",
-  gid  => $group, 
+  $user = $title,
+  $group= $title,
+  )
+  {
+  
+  user{ "${title}":
+     ensure => present,
   }
-
-group { $group :
-    ensure => present,
-  }
-
-file { "/home/${user}":
-ensure => directory,
-owner => $user,
-group => $group,
-}
-}
-
-
+  
+  file{ "/home/${title}":
+    ensure => directory,
+    owner => $title,
+    group => $title,
+    }
+ }
 
