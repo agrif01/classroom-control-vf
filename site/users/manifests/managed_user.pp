54291dca737b123708e4027193dbe298f80beb6b
skura01 <sireesha.kurapati@safeway.com>
2017-02-09 10:22:25 -0800

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,7 @@
 define users::managed_user (
 $user = $input1, $group = $input2
-) {
-user { $input1:
-ensure => present,
-}
-user { $input2:
-ensure => present,
-}
+)
+{
 file { "/home/${input1}":
 ensure => directory,
 owner => $input1,
