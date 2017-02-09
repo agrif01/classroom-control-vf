f9f4c9de8a8ffb73c9b3048ab46ed734bbfa2e22
acapi12016 <aecapigon@gmail.com>
2017-02-10 00:56:11 +0800

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,19 +1,19 @@
-class skeleteon {
+class skeleton {
 
-  file {'/etc/skel';
+  file { '/etc/skel' :
     ensure => directory,
-    owner => root,
-    group => 'root',
-    mode => '@755',
-  
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
   
-  file {'etc/skel/.bahsrc';
+  file { '/etc/skel/.bashrc' :
     ensure => file,
-    owner => root,
-    group => 'root',
-    mode = '@644',
-    source=>'puppet:///modules/skeleton/bashrc',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
   }
-  
-  }
+
+
+}
