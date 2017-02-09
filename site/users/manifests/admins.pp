9b029802eb574efdc447690da8a0375e131316f9
skura01 <sireesha.kurapati@safeway.com>
2017-02-09 10:36:41 -0800

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,9 +1,13 @@
 class users::admins {
-users::managed_user { 'joe','wls' }
-users::managed_user { 'alice', 'wls'}
-users::managed_user { 'chen', 'wls'}
+users::managed_user { 'joe','wls' : }
+users::managed_user { 'alice', 'wls' : }
+users::managed_user { 'chen', 'wls' : }
 
 group { 'wls':
 ensure => present,
 }
 }
+
+
+
+
