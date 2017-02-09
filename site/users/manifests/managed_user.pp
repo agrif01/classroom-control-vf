6328fc9690d6a879d3f19e281e981eaa3ca3f991
agrif01 <agrif01@safeway.com>
2017-02-09 11:02:42 -0700

    exercise 15.1

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,13 @@
+define user::managed_user (
+  $group= ${title},
+  )
+  {
+  user{ ${title}:
+     ensure => present,
+  }
+  file{"/home/${title}":
+    ensure => directory
+    owner => ${title},
+    group => ${title},
+    }
+  }
