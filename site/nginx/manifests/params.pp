ca48a0df6e8a4634b4d8521fce44e6026bf172df
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-09 14:00:06 -0800

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,4 +1,25 @@
 class nginx::params {
-
+case $facts['os']['family'] {
+  'RedHat', 'Debian' : {
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $blockdir = '/etc/nginx/conf.d'
+    $logdir   = '/var/log/nginx'
+  }
+  'windows' : {
+    $package  = 'nginx-service'
+    $owner    = 'Administrator'
+    $group    = 'Administrators'
+    $docroot  = 'C:/ProgramData/nginx/html'
+    $confdir  = 'C:/ProgramData/nginx'
+    $blockdir = 'C:/ProgramData/nginx/conf.d'
+    $logdir   = 'C:/ProgramData/nginx/logs'
+  }
+  default: { 
+    fail("Operating system is not supported by this module")
+  }
 
 }
