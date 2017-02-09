c9a5ec8177c6744b0d90f3e732724aa5b3ee0d93
Shobhna Shastri <shobhna@puppet.com>
2017-02-09 15:50:34 -0800

    Removed ghost

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,9 +3,5 @@ class profile::apache {
   class { 'apache': }
   include apache::mod::php
   
-  apache::vhost { 'vhost.example.com':
-    port    => '80',
-    docroot => '/opt/wordpress',
-  }
   
 }
