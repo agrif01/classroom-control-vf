ba4a87343a1dc093c45f96d5292bff208a04d9a6
Shobhna Shastri <shobhna@puppet.com>
2017-02-09 10:28:05 -0800

    Update managed user

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,16 +1,21 @@
-define users::managed_user {  
+define users::managed_user ( 
 $user = $title,
 $group = $title,
-} {
+) {
 
   user { $user : 
   ensure=> present,
   home => "/home/${user}",
+  gid  => $group, 
   }
   
   file { "/home/${user}" ,
    ensure => directory,
    owner => $user,
-   group => $group
+   group => $group,
+  }
+  
+  group { $group :
+    ensure => present,
   }
 }
