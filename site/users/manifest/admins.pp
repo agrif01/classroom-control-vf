fd1b1f9c6aa6b367a7e6735a8ae42fa117ccc518
acapi12016 <aecapigon@gmail.com>
2017-02-10 02:42:50 +0800

    Update admins.pp

--- site/users/manifest/admins.pp
+++ site/users/manifest/admins.pp
@@ -1,13 +1,7 @@
-#class users::admins {
-
-  #user {'admin':
-  #  ensure => present,
-  #  }
-  # }
 
 
 class users::admins {
-users::managed_user { 'joe': }
-users::managed_user { 'alice': }
-users::managed_user { 'chen': }
+      users::managed_user { 'joe': }
+      users::managed_user { 'alice': }
+      users::managed_user { 'chen': }
 }
