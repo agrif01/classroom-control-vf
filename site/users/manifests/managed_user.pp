dbd81b8b0df6ce364ecc8754ad8e65833ab640a2
agrif01 <agrif01@safeway.com>
2017-02-09 11:28:29 -0700

    update 1

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,13 +1,15 @@
 define user::managed_user (
+  $user = ${title},
   $group= ${title},
   )
-  {
+  
   user{ ${title}:
      ensure => present,
   }
+  
   file{"/home/${title}":
     ensure => directory
     owner => ${title},
     group => ${title},
     }
-  }
+ 
