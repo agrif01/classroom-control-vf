14e37ba807c81a5b14c91cea7ac08bc09a1b44b5
Shobhna Shastri <shobhna@puppet.com>
2017-02-09 15:46:27 -0800

    Changing docroot for wordpress

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,4 +3,9 @@ class profile::apache {
   class { 'apache': }
   include apache::mod::php
   
+  apache::vhost { 'vhost.example.com':
+    port    => '80',
+    docroot => '/opt/wordpress',
+  }
+  
 }
