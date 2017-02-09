d470fbf8f8caa4732926d639f920a32265f02eee
skura01 <sireesha.kurapati@safeway.com>
2017-02-09 09:56:50 -0800

    Lab 15.1

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,7 +1,12 @@
 class users::admins {
-
-  user { 'admin':
-    ensure => present,
-  }
-
+users::managed_user { 'joe': }
+users::managed_user { 'alice':
+group => 'staff',
+}
+users::managed_user { 'aaron':
+group => 'staff',
+}
+group { 'staff':
+ensure => present,
+}
 }
