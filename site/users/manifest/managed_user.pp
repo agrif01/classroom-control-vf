bd1a948696650c352ae8be1844d59c224232fae5
acapi12016 <aecapigon@gmail.com>
2017-02-10 02:42:11 +0800

    Update managed_user.pp

--- site/users/manifest/managed_user.pp
+++ site/users/manifest/managed_user.pp
@@ -1,12 +1,14 @@
 define users::managed_user (
 $group = $title,
-) {
-user { $title:
-ensure => present,
-}
-file { "/home/${title}":
-ensure => directory,
-owner => $title,
-group => $group,
-}
+) 
+
+{
+  user { $title:
+  ensure => present,
+  }
+  file { "/home/${title}":
+  ensure => directory,
+  owner => $title,
+  group => $group,
+  }
 }
