0d09a0eb8fef0bd3c533e9ddea3264cfa700a076
hcoro03 <Hero.Coro@safeway.com>
2017-02-10 02:02:22 +0800

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,6 +1,12 @@
 class users::admins {
-
-  user { 'admin':
+  users::managed_user {'joe':}
+  users::managed_user {'alice':
+    group => 'staff',
+  }
+  users::managed_user {'aaron':
+    group => 'staff',
+  }
+  group {'staff':
     ensure => present,
   }
 
