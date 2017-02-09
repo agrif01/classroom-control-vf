1bca9cfd635043c63e4c5044ac58577d3430067a
agrif01 <agrif01@safeway.com>
2017-02-09 07:37:48 -0700

    exercise 13.2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,78 @@
 class nginx {
 
+case $facts['os']['family'] {
+	'debian': {
+		$pkgname => 'nginx' 
+		$fowner => 'root' 
+		$fgroup  => 'root'
+		$docroot  => '/var/www' 
+		$configdir  => '/etc/nginx/' 
+		$svrblkdir  => '/etc/nginx/conf.d'
+		$logdir  => '/var/log/nginx' 
+		$svcname  => 'nginx' 
+		$usrsrvrunas =>  'www-data' 
+	}
+	'windows': {
+		$kgname =>  'nginx-service'
+		$fowner =>  'Administrator'
+		$fgroup  => 'Administrators'
+		$docroot  =>  'C:/ProgramData/nginx/html'
+		$configdir  =>  'C:/ProgramData/nginx'
+		$svrblkdir  => 'C:/ProgramData/nginx/conf.d'
+		$logdir  =>  'C:/ProgramData/nginx/logs'
+		$svcname  =>  'nginx'
+		$usrsrvrunas => 'nobody'
+	}
+	'redhat': {
+		$pkgname => 'nginx' 
+		$fowner => 'root' 
+		$fgroup  => 'root'
+		$docroot  => '/var/www' 
+		$configdir  => '/etc/nginx/' 
+		$svrblkdir  => '/etc/nginx/conf.d'
+		$logdir  => '/var/log/nginx' 
+		$svcname  => 'nginx'
+		$usrsrvrunas => 'nginx' 
+	}
+	default: {
+		fail("Operating system family ${facts['os']['family']} is not supported.")
+	}
+}
+
 FILE {
-        owner => 'root',
-        group =>' root' ,
+        owner => '${fowner}',
+        group => '${fgroup}' ,
         mode => '0664' ,
      }
-  package { 'nignx':
+  package { '${pkgname}':
         ensure => present,
      {
-  file { '/var/www' :
+  file { '${docroot} :
         ensure => directory,
         mode => '0755' ,
      }
- file { '/var/www/index.html' :
+ file { '${docroot}/index.html' :
         ensure => file,
         source =>'puppet:///modules/nginx/index.html' ,
      }
- file { '/etc/nginx/nginx.conf':
+ file { '${configdir}/nginx.conf':
        ensure => file ,
         source => 'puppet:///modules/nginx/nginx.conf' ,
        require => Package['nginx'] ,
        notify => Service['nginx'] ,
     }
     
-   file { '/etc/nginx/conf.d':
+   file { '${svrblkdir}':
    ensure => directory,
    mode => '0775',
    }
-  file { '/etc/nginx/conf.d/default.conf':
+  file { '${svrblkdir}/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
+    require => Package['${pkgname}'],
+    notify => Service['${svcname}'],
   }
-  service { 'nginx':
+  service { '${svcname}':
    ensure => running,
    enable => true,
   }
