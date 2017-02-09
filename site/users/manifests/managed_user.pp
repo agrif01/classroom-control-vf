852b5cc8de2ec7b1781ceaa84864f17ddddacf30
shawnh13 <shawn@duchessblue.com>
2017-02-09 11:04:05 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,14 @@
+define users::managed_user {
+  $user = $title,
+  $homedir = /home/$title,
+  $groups = 'users',
+  $ssh_dir = "/home/${title}/.ssh",
+  $shell = "/bin/bash",
+} {
+  user { "${user}" :
+    ensure  => present,
+    name    => "${user}",
+    home    => "${homedir}",
+    groups  => "${groups}",
+    shell   => "${shell}",
+}
