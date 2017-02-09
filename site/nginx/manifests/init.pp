32dbc1575ca0e65c8706274e17f2e8da4e2d54b8
Jmari43 <jyothi.marisetti@safeway.com>
2017-02-09 14:02:30 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,15 @@
 class nginx {
 
+ $package  = $nginx::params::package,
+    $owner    = $nginx::params::owner,
+    $group    = $nginx::params::group,
+    $docroot  = $nginx::params::docroot,
+    $confdir  = $nginx::params::confdir,
+    $blockdir = $nginx::params::blockdir,
+    $logdir   = $nginx::params::logdir,
+    $user     = $nginx::params::user,
+) inherits nginx::params {
+
 File {
   owner  => 'root',
   group  => 'root',
